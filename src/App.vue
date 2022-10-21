025096b7ace80ad63205247fccc6df2b4ad38371
Kristina Tuzova <ktuzova1901@gmail.com>
2022-10-21 13:39:47 +0300

    cod

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,80 @@
 <template>
-  <Btn>Create</Btn>
-</template>
-
+  <div class="app">
+    <form @submit.prevent>
+      <h4>Создание поста</h4>
+      <input type="text" placeholder="Название" @input="title = $event.target.value" v-bind:value="title"/>
+      <input type="text" placeholder="Описание" @input="body = $event.target.value" v-bind:value="body"/>
+      <button class="btn" @click="createPost">Создать</button>
+    </form>
+    <div class="post" v-for="post in posts" v-bind:key="post.id"> 
+      <div>
+        <strong>Название:</strong>
+        {{post.title}}
+     </div>
+     <div>
+        <strong>Описание:</strong>
+        {{post.body}}
+     </div>
+     </div>
+     </div>
+ </template>
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+ box-sizing: border-box;
+}
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+input {
+  width: 100%;
+  border: 1px solif teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+.app {
+  padding: 20px;
+}
+form {
+  display: flex;
+  flex-direction: column;
+}
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+</style>
 <script>
-import Btn from './components/Button';
-
 export default {
-  name: 'App',
-  components: {
-    Btn
+  data() {
+    return {
+      posts: [
+        {id: 1, title: "JavaScript", body: "Описание поста"},
+        {id: 2, title: "JavaScript2", body: "Описание поста2"},
+        {id: 2, title: "JavaScript2", body: "Описание поста2"},
+      ],
+      title: '',
+      body: ''
+    }
+  },
+  methods: {
+     createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body
+      }
+    this.posts.push(newPost)
+    this.title = ''
+    this.body = ''
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/Button.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-const Button = Vue.component('btn-item', {
-    render(createElement) {
-       return createElement('button', this.$slots.default)
-    }
-})
-export default Button
